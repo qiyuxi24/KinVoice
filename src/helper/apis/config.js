36acd009a3cb0dcc54ba88cc69e2e8d636174172
Blokki <121482218+wdsj2127@users.noreply.github.com>
2026-06-19 23:51:06 +0800

    小修复
    修改端口和gitignore

--- src/helper/apis/config.js
+++ src/helper/apis/config.js
@@ -1,7 +1,7 @@
-const previewBaseUrl = 'http://127.0.0.1:18000'
+const previewBaseUrl = 'http://127.0.0.1:8000'
 
 export default {
-  // QuickApp Studio 本机预览默认走 18000 端口。
+  // QuickApp Studio 本机预览默认走 8000 端口。
   // 若切到扫码或 USB 真机调试，请改成电脑的局域网 IP，例如 http://192.168.1.8:18000。
   baseUrl: previewBaseUrl,
 }
